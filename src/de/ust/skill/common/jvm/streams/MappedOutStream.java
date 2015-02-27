4b6684acd15f1fe7c2795419af4f3e7e937c4ed0
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-02-27 18:05:31 +0100

    removed buffer check in mapped outstream for v64

--- src/de/ust/skill/common/jvm/streams/MappedOutStream.java
+++ src/de/ust/skill/common/jvm/streams/MappedOutStream.java
@@ -23,4 +23,63 @@ final public class MappedOutStream extends OutStream {
 	protected void refresh() throws IOException {
 		// do nothing; let the JIT remove this method and all related checks
 	}
+
+    @Override
+    public final void v64(long v) throws IOException {
+    	if (0L == (v & 0xFFFFFFFFFFFFFF80L)) {
+    		buffer.put((byte) v);
+    	} else if (0L == (v & 0xFFFFFFFFFFFFC000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (v >> 7));
+    	} else if (0L == (v & 0xFFFFFFFFFFE00000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (v >> 14));
+    	} else if (0L == (v & 0xFFFFFFFFF0000000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (v >> 21));
+    	} else if (0L == (v & 0xFFFFFFF800000000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (0x80L | v >> 21));
+    		buffer.put((byte) (v >> 28));
+    	} else if (0L == (v & 0xFFFFFC0000000000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (0x80L | v >> 21));
+    		buffer.put((byte) (0x80L | v >> 28));
+    		buffer.put((byte) (v >> 35));
+    	} else if (0L == (v & 0xFFFE000000000000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (0x80L | v >> 21));
+    		buffer.put((byte) (0x80L | v >> 28));
+    		buffer.put((byte) (0x80L | v >> 35));
+    		buffer.put((byte) (v >> 42));
+    	} else if (0L == (v & 0xFF00000000000000L)) {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (0x80L | v >> 21));
+    		buffer.put((byte) (0x80L | v >> 28));
+    		buffer.put((byte) (0x80L | v >> 35));
+    		buffer.put((byte) (0x80L | v >> 42));
+    		buffer.put((byte) (v >> 49));
+    	} else {
+    		buffer.put((byte) (0x80L | v));
+    		buffer.put((byte) (0x80L | v >> 7));
+    		buffer.put((byte) (0x80L | v >> 14));
+    		buffer.put((byte) (0x80L | v >> 21));
+    		buffer.put((byte) (0x80L | v >> 28));
+    		buffer.put((byte) (0x80L | v >> 35));
+    		buffer.put((byte) (0x80L | v >> 42));
+    		buffer.put((byte) (0x80L | v >> 49));
+    		buffer.put((byte) (v >> 56));
+    	}
+    }
 }
--- src/de/ust/skill/common/jvm/streams/OutStream.java
+++ src/de/ust/skill/common/jvm/streams/OutStream.java
@@ -9,34 +9,33 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /**
- * Encapsulation of memory mapped files that allows to use skill types and mmap
- * efficiently.
+ * Encapsulation of memory mapped files that allows to use skill types and mmap efficiently.
  *
  * @author Timm Felden
  */
 abstract public class OutStream {
 
-	// the buffer size is only used for mapping headers, which are usually
-	// smaller
-	protected final int BUFFERSIZE;
-	protected ByteBuffer buffer;
+    // the buffer size is only used for mapping headers, which are usually
+    // smaller
+    protected final int BUFFERSIZE;
+    protected ByteBuffer buffer;
 
-	// current write position
-	protected long position = 0L;
+    // current write position
+    protected long position = 0L;
 
-	public long position() {
-		if (null == buffer)
-			return position;
+    public long position() {
+        if (null == buffer)
+            return position;
 
-		return buffer.position() + position;
-	}
+        return buffer.position() + position;
+    }
 
-	protected OutStream(ByteBuffer buffer) {
-		this.buffer = buffer;
-		BUFFERSIZE = buffer.capacity();
-	}
+    protected OutStream(ByteBuffer buffer) {
+        this.buffer = buffer;
+        BUFFERSIZE = buffer.capacity();
+    }
 
-	protected abstract void refresh() throws IOException;
+    protected abstract void refresh() throws IOException;
 
     final public void bool(boolean data) throws IOException {
         if (null == buffer || buffer.position() == BUFFERSIZE)
@@ -50,94 +49,35 @@ abstract public class OutStream {
         buffer.put(data);
     }
 
-	final public void i16(short data) throws IOException {
-		if (null == buffer || buffer.position() + 2 > BUFFERSIZE)
-			refresh();
-		buffer.putShort(data);
-	}
+    final public void i16(short data) throws IOException {
+        if (null == buffer || buffer.position() + 2 > BUFFERSIZE)
+            refresh();
+        buffer.putShort(data);
+    }
 
-	final public void i32(int data) throws IOException {
-		if (null == buffer || buffer.position() + 4 > BUFFERSIZE)
-			refresh();
-		buffer.putInt(data);
-	}
+    final public void i32(int data) throws IOException {
+        if (null == buffer || buffer.position() + 4 > BUFFERSIZE)
+            refresh();
+        buffer.putInt(data);
+    }
 
-	final public void i64(long data) throws IOException {
-		if (null == buffer || buffer.position() + 8 > BUFFERSIZE)
-			refresh();
-		buffer.putLong(data);
-	}
+    final public void i64(long data) throws IOException {
+        if (null == buffer || buffer.position() + 8 > BUFFERSIZE)
+            refresh();
+        buffer.putLong(data);
+    }
 
-	final public void v64(long v) throws IOException {
-		if (null == buffer || buffer.position() + 9 >= BUFFERSIZE)
-			refresh();
+    abstract public void v64(long data) throws IOException;
 
-		if (0L == (v & 0xFFFFFFFFFFFFFF80L)) {
-			buffer.put((byte) v);
-		} else if (0L == (v & 0xFFFFFFFFFFFFC000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (v >> 7));
-		} else if (0L == (v & 0xFFFFFFFFFFE00000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (v >> 14));
-		} else if (0L == (v & 0xFFFFFFFFF0000000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (v >> 21));
-		} else if (0L == (v & 0xFFFFFFF800000000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (0x80L | v >> 21));
-			buffer.put((byte) (v >> 28));
-		} else if (0L == (v & 0xFFFFFC0000000000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (0x80L | v >> 21));
-			buffer.put((byte) (0x80L | v >> 28));
-			buffer.put((byte) (v >> 35));
-		} else if (0L == (v & 0xFFFE000000000000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (0x80L | v >> 21));
-			buffer.put((byte) (0x80L | v >> 28));
-			buffer.put((byte) (0x80L | v >> 35));
-			buffer.put((byte) (v >> 42));
-		} else if (0L == (v & 0xFF00000000000000L)) {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (0x80L | v >> 21));
-			buffer.put((byte) (0x80L | v >> 28));
-			buffer.put((byte) (0x80L | v >> 35));
-			buffer.put((byte) (0x80L | v >> 42));
-			buffer.put((byte) (v >> 49));
-		} else {
-			buffer.put((byte) (0x80L | v));
-			buffer.put((byte) (0x80L | v >> 7));
-			buffer.put((byte) (0x80L | v >> 14));
-			buffer.put((byte) (0x80L | v >> 21));
-			buffer.put((byte) (0x80L | v >> 28));
-			buffer.put((byte) (0x80L | v >> 35));
-			buffer.put((byte) (0x80L | v >> 42));
-			buffer.put((byte) (0x80L | v >> 49));
-			buffer.put((byte) (v >> 56));
-		}
-	}
+    final public void f32(float data) throws IOException {
+        if (null == buffer || buffer.position() + 4 > BUFFERSIZE)
+            refresh();
+        buffer.putFloat(data);
+    }
 
-	final public void f32(float data) throws IOException {
-		if (null == buffer || buffer.position() + 4 > BUFFERSIZE)
-			refresh();
-		buffer.putFloat(data);
-	}
-
-	final public void f64(double data) throws IOException {
-		if (null == buffer || buffer.position() + 8 > BUFFERSIZE)
-			refresh();
-		buffer.putDouble(data);
-	}
+    final public void f64(double data) throws IOException {
+        if (null == buffer || buffer.position() + 8 > BUFFERSIZE)
+            refresh();
+        buffer.putDouble(data);
+    }
 }
