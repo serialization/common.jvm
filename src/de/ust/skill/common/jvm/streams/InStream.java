75f9b2945933da5fbcf3f81cc89de67cebb6184e
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-10-06 16:00:13 +0200

    jvm friendly v64 implementation

--- src/de/ust/skill/common/jvm/streams/InStream.java
+++ src/de/ust/skill/common/jvm/streams/InStream.java
@@ -8,8 +8,7 @@ package de.ust.skill.common.jvm.streams;
 import java.nio.ByteBuffer;
 
 /**
- * Implementations of this class are used to turn a byte stream into a stream of
- * integers and floats.
+ * Implementations of this class are used to turn a byte stream into a stream of integers and floats.
  *
  * @author Timm Felden
  */
@@ -18,88 +17,118 @@ public abstract class InStream {
 
     protected InStream(ByteBuffer input) {
         this.input = input;
-	}
+    }
 
-	/**
-	 * @return take an f64 from the stream
-	 */
+    /**
+     * @return take an f64 from the stream
+     */
     final public double f64() {
         return input.getDouble();
     }
 
-	/**
-	 * @return take an f32 from the stream
-	 */
+    /**
+     * @return take an f32 from the stream
+     */
     public final float f32() {
         return input.getFloat();
     }
 
-	/**
-	 * @return take an v64 from the stream
-	 */
+    /**
+     * @return take an v64 from the stream
+     */
     public final long v64() {
-        long count = 0;
-        long rval = 0;
-        long r = i8();
-        while (count < 8 && 0 != (r & 0x80)) {
-            rval |= (r & 0x7f) << (7 * count);
+        long rval;
 
-            count += 1;
-            r = i8();
+        return (0 != ((rval = i8()) & 0x80)) ? multiByteV64(rval) : rval;
+    }
+
+    /**
+     * multi byte v64 values are treated in a different function to enable inlining of more common single byte v64
+     * values
+     */
+    @SuppressWarnings("all")
+    private long multiByteV64(long rval) {
+        long r;
+        rval |= ((r = i8()) & 0x7f) << 7;
+
+        if (0 != (r & 0x80)) {
+            rval |= ((r = i8()) & 0x7f) << 14;
+
+            if (0 != (r & 0x80)) {
+                rval |= ((r = i8()) & 0x7f) << 21;
+
+                if (0 != (r & 0x80)) {
+                    rval |= ((r = i8()) & 0x7f) << 28;
+
+                    if (0 != (r & 0x80)) {
+                        rval |= ((r = i8()) & 0x7f) << 35;
+
+                        if (0 != (r & 0x80)) {
+                            rval |= ((r = i8()) & 0x7f) << 42;
+                            if (0 != (r & 0x80)) {
+                                rval |= ((r = i8()) & 0x7f) << 49;
+
+                                if (0 != (r & 0x80)) {
+                                    rval |= i8() << 56;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        rval = (rval | (8 == count ? r : (r & 0x7f)) << (7 * count));
         return rval;
     }
 
-	/**
-	 * @return take an i64 from the stream
-	 */
+    /**
+     * @return take an i64 from the stream
+     */
     public final long i64() {
         return input.getLong();
     }
 
-	/**
-	 * @return take an i32 from the stream
-	 * @throws UnexpectedEOF
-	 *             if there is no i32 in the stream
-	 */
+    /**
+     * @return take an i32 from the stream
+     * @throws UnexpectedEOF
+     *             if there is no i32 in the stream
+     */
     public final int i32() {
         return input.getInt();
     }
 
-	/**
-	 * @return take an i16 from the stream
-	 */
+    /**
+     * @return take an i16 from the stream
+     */
     public final short i16() {
         return input.getShort();
     }
 
-	/**
-	 * @return take an i8 from the stream
-	 */
+    /**
+     * @return take an i8 from the stream
+     */
     public final byte i8() {
         return input.get();
     }
 
-	/**
-	 * @return take a bool from the stream
-	 */
+    /**
+     * @return take a bool from the stream
+     */
     public final boolean bool() {
         return input.get() != 0;
     }
 
-	/**
-	 * @return raw byte array taken from the stream
-	 */
+    /**
+     * @return raw byte array taken from the stream
+     */
     public final byte[] bytes(long length) {
         final byte[] rval = new byte[(int) length];
         input.get(rval);
         return rval;
     }
 
-	/**
-	 * @return true iff there are at least n bytes left in the stream
-	 */
+    /**
+     * @return true iff there are at least n bytes left in the stream
+     */
     public final boolean has(int n) {
         return input.limit() >= n + input.position();
     }
@@ -111,14 +140,13 @@ public abstract class InStream {
         return input.limit() == input.position();
     }
 
-	/**
-	 * use with care!
-	 * 
-	 * @param position
-	 *            jump to target position, without the ability to restore the
-	 *            old position
-	 */
-	public abstract void jump(long position);
+    /**
+     * use with care!
+     * 
+     * @param position
+     *            jump to target position, without the ability to restore the old position
+     */
+    public abstract void jump(long position);
 
     final public long position() {
         return input.position();
